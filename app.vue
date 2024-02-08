add5de5767934c14e6090222a60696a777ebbbb7
Erik Thalén <erik.thalen@hyperisland.se>
2024-02-08 22:15:27 +0100

    init commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    data:
+    <code><pre>{{ data }}</pre></code>
+  </div>
+</template>
+
+<script setup>
+const data = await queryContent('/posts/hello-world').findOne()
+
+console.log(data)
+</script>
